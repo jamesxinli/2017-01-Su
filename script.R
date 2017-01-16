d547c8ac5cc773bf87489e6e3ccffc602bfc03c8
James Li <james@Jamess-MacBook-Pro.local>
2017-01-16 17:51:41 -0500

    commit1
    First commit to this R file

---
+++ script.R
@@ -0,0 +1,11 @@
+setwd("C:/James/Box Sync/Work/Consulting/2017-01-Su")
+x<-read.csv("dotplot-regression.csv")
+
+plot(cbind(x[2],x[1]),pch=19,cex=1.5)
+abline(lm(x$NR4A2~x$SNCA),col="blue",lwd=5)
+lines(lowess(x$SNCA,x$NR4A2),col="red",lwd=5)
+
+
+sink("regression.txt")
+summary(lm(x$NR4A2~x$SNCA))
+sink()
