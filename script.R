26cbc8a03cdbabfbbec7bebd455ce14e73e90064
jamesxinli <james@Jamess-MacBook-Pro.local>
2017-09-11 13:17:06 -0400

    Test

--- script.R
+++ script.R
@@ -5,8 +5,7 @@ plot(cbind(x[2],x[1]),pch=19,cex=1.5)
 abline(lm(x$NR4A2~x$SNCA),col="blue",lwd=5)
 lines(lowess(x$SNCA,x$NR4A2),col="red",lwd=5)
 
-
-
+# Export the results
 sink("regression.txt")
 summary(lm(x$NR4A2~x$SNCA))
 sink()
